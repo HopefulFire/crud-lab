016fc5fe159cf5cbfb787b5b8bbd02826ea4073f
Jade Lynn Masker <jadelynn@masker.family>
2021-03-24 21:41:51 -0400

    commit tree 83933c64f73cd07d684abfeceda697d478332b02

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { connect } from "react-redux"
 
 class RestaurantInput extends Component {
+
+  state = {
+    text: ""
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        Restaurant Input
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.text}
+            name="text"
+            onChange={this.handleChange}
+          />
+        </form>
       </div>
-    );
+    )
   }
-};
+}
 
-export default RestaurantInput;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addRestaurant: (text) => dispatch({
+      type: "ADD_RESTAURANT",
+      payload: text
+    })
+  }
+}
+
+export default connect(null, mapDispatchToProps)(RestaurantInput)
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,5 +1,24 @@
 import cuid from 'cuid';
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state={
+  restaurants: [],
+  reviews: [],
+}, action) {
+  switch (action.type) {
+    case "ADD_RESTAURANT": {
+      return {
+        ...state,
+        restaurants: [
+          ...state.restaurants,
+          {
+            text: action.payload,
+            id: cuid(),
+          },
+        ],
+      }
+    }
+    default: {
+      return state
+    }
+  }
 }
